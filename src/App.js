005f2fb663119801b0aaf44f70dbeac743073ecf
yinm <yinm.cminor9@gmail.com>
2018-05-26 13:44:52 +0900

    Add Image component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header/index'
+import { Image } from './components/Image/index'
 import './normalize.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Header/>
+      <div>
+        <Header/>
+        <Image/>
+      </div>
     );
   }
 }
---
+++ src/components/Image/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class Image extends Component {
+  render() {
+    return (
+      <div className="dummy-image"></div>
+    )
+  }
+}
