06718f504a62d4fc0f36f291916377917e41ff28
yinm <yinm.cminor9@gmail.com>
2018-05-26 16:12:29 +0900

    Add style

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import styles from './styles.css'
 
 export class Footer extends Component {
   render() {
     return (
-      <footer>
-        <span>© 2017 SUMIRE HIBIYA</span>
+      <footer className="App-footer">
+        <p>© 2017 SUMIRE HIBIYA</p>
       </footer>
     )
   }
