db20f5654d17791309c14c137c31116249c2f94a
yinm <yinm.cminor9@gmail.com>
2018-05-26 13:40:08 +0900

    Add styles (TODO: navの位置をタイトルに合わせる)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react'
+import styles from './styles.css'
 
 export class Header extends Component {
   render() {
     return (
-      <header>
-        <p>UI DESIGNER</p>
-        <h1><a href="#">SUMIRE HIBIYA</a></h1>
+      <header className="App-header">
+        <div className="title">
+          <p className="App-subtitle">UI DESIGNER</p>
+          <h1 className="App-title"><a href="#">SUMIRE HIBIYA</a></h1>
+        </div>
 
-        <nav>
-          <ul>
-            <li><a href="#">TOP</a></li>
-            <li><a href="#">BLOG</a></li>
+        <nav className="nav">
+          <ul className="nav-table">
+            <li className="nav-cell"><a href="#">TOP</a></li>
+            <li className="nav-cell"><a href="#">BLOG</a></li>
           </ul>
         </nav>
       </header>
