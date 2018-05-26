1f579c5f2dbc10c00e5817b6f0b7214c6d437494
yinm <yinm.cminor9@gmail.com>
2018-05-26 17:14:29 +0900

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header'
 import { Main } from './components/Main'
+import { Footer } from './components/Footer'
 
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div>
         <Header/>
         <Main/>
+        <Footer/>
       </div>
     )
   }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <p className="footer">© 2017 SUMIRE HIBIYA</p>
+    )
+  }
+}
