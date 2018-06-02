ea4046691445a5317dc5d36043c89d27de547d9e
yinm <yinm.cminor9@gmail.com>
2018-06-02 17:45:07 +0900

    Add HTML of TOC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header'
 import { Article } from './components/Article'
+import { TableOfContents } from './components/TableOfContents'
 import { Footer } from './components/Footer'
 
 import './App.css';
@@ -12,6 +13,11 @@ class App extends Component {
         <Header/>
         <div className="l-blog__wrapper">
           <Article />
+          <div className="l-blog__sub p-blogSidebar">
+            <div className="p-blogSidebar__outer" style={{ position: 'relative', top: '0px' }}>
+              <TableOfContents/>
+            </div>
+          </div>
         </div>
         <Footer/>
       </div>
