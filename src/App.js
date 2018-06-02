00412cb89084144bb8be30f713411ac800bd3cc3
yinm <yinm.cminor9@gmail.com>
2018-06-02 16:14:05 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
-import { Article } from './components/Article'
 
 import './App.css';
 
@@ -10,14 +9,6 @@ class App extends Component {
     return (
       <div>
         <Header/>
-        <main className="l-article__main">
-          <Article/>
-          <div style={{
-            backgroundColor: '#999',
-            width: '300px',
-          }}>
-          </div>
-        </main>
         <Footer/>
       </div>
     )
