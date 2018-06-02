381187bf2d8041d1c0048f276373f3d395511acb
yinm <yinm.cminor9@gmail.com>
2018-06-02 16:40:36 +0900

    Add HTML

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header'
+import { Article } from './components/Article'
 import { Footer } from './components/Footer'
 
 import './App.css';
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div>
         <Header/>
+        <Article />
         <Footer/>
       </div>
     )
